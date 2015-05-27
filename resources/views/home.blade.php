3546d2b1a01dd6a387ae3ab514ba3f46aff5b51c
popeJohnPakart <johncordova24@gmail.com>
2015-05-27 17:00:32 +0800

    try

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 <div class="container">
 	<div class="row">
+		<div></div>
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-default">
 				<div class="panel-heading">Home</div>
